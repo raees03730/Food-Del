77c4219c4823eedba648a2e227038d1e3671ba28
raees03730 <ransari60212@gmail.com>
2026-02-25 15:22:00 +0530

    Update API URL to production endpoint

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://food-del-backend-aima.onrender.com"
     
     // ✅ FIX 1: Initial state localStorage se lo
     const [token, setToken] = useState(localStorage.getItem("token") || "");
